81c23a50f4bd3761a1dcf76972015ab1ad1c34a3
GoncharenkoAI <GoncharenkoAI@DESKTOP-7QEUBPN>
2022-07-10 17:51:27 +0700

    Добавьте файлы проекта.

---
+++ ControlSystem/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlSystem
+{
+    internal class Task
+    {
+        public static int ID_Game { get; set; }
+        public static string Name { get; set; }
+        public static decimal Price { get; set; }
+        public static decimal Discount { get; set; }
+        public static string Genre { get; set; }
+    }
+}
